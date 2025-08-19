1229fe98cbcd67d16326e308973e4b2b6c434048
juop12 <sadeijuop12@gmail.com>
2025-08-19 23:51:02 +0200

    Add: redefino el tipo de dato, creo insertar y CHANGELOG

--- haskell/abb.hs
+++ haskell/abb.hs
@@ -1,8 +1,16 @@
-{- DEF data structure -}
-data (Ord a, Eq a) => ABB a = Nil | Bin (ABB a) a (ABB a) 
-	deriving Show
 
-ab1 = Bin (Bin (Bin Nil 1 Nil) 3 Nil) 5 (Bin (Bin Nil 7 Nil) 8 (Bin Nil 9 Nil))
+-- Defino el tipo de dato abstracto (TDA) de Arbol Binario de Busqueda
+data ABB a = Nil | Bin (ABB a) a (ABB a) deriving Show
+
+abb1 = Bin (Bin (Bin Nil 1 Nil) 3 Nil) 5 (Bin (Bin Nil 7 Nil) 8 (Bin Nil 9 Nil))
+abb2 = Bin (Bin Nil -1 (Bin Nil 0 (Bin Nil 1 Nil))) 1 (Bin (Bin Nil 2 Nil) 3 Nil)
+
+insertar :: (Ord a) => ABB a -> a -> ABB a
+insertar Nil valor = Bin Nil valor Nil
+insertar (Bin izq raiz der) valor 
+    | valor <= raiz = Bin (insertar izq valor) raiz der
+    | valor > raiz = Bin izq raiz (insertar der valor)
+ -- | otherwise = Bin l y r -- x == y, no change needed
 
 preorder :: ABB a -> [a]
 preorder Nil = []
@@ -15,3 +23,9 @@ inorder (Bin izq raiz der) = inorder izq ++ (raiz : inorder der)
 postorder :: ABB a -> [a]
 postorder Nil = []
 postorder (Bin izq raiz der) = postorder izq ++ (postorder der ++ [raiz])
+
+-- CHANGELOG:
+    -- Los constarints al tipo de dato dependiente de un generico van en las implementaciones de las funciones, no en las definiciones de los TDAs.
+    -- El Impl de Ord ya contiene Eq por defecto.
+    -- Si inserto un elemento ya existente, lo inserto como predecesor inorder.
+    -- Si empiezo insertando Enteros, puedo insertar un Float luego y lo transforma.
