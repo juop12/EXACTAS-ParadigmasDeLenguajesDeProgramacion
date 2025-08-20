8f05722ce1bec78cabb58b32ac2a2c0466f42723
juop12 <sadeijuop12@gmail.com>
2025-08-20 03:30:44 +0200

    Add: Agrego los ejercicios de Sets

---
+++ haskell/set.hs
@@ -0,0 +1,11 @@
+newtype Conj a = CConj [a]
+
+-- vacio :: Conj a
+
+-- insertar :: Eq a => a -> Conj a -> Conj a
+
+-- pertenece :: Eq a => a -> Conj a -> Bool
+
+-- eliminar :: Eq a => a -> Conj a -> Conj a
+
+-- Show??
