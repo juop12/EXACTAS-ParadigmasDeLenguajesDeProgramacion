b1c6cc60ac552baacbef7ff86292f9aff8eabd68
juop12 <sadeijuop12@gmail.com>
2025-08-24 05:19:20 +0200

    Add: Se agrega eliminar al Set

--- haskell/Set.hs
+++ haskell/Set.hs
@@ -1,3 +1,5 @@
+import Data.List (delete)
+
 newtype Conj a = CConj [a]
 
 vacio :: Conj a
@@ -6,12 +8,16 @@ vacio = CConj []
 insertar :: Eq a => a -> Conj a -> Conj a
 insertar v (CConj lista) = CConj (if v `elem` lista then lista else v : lista)
 
--- pertenece :: Eq a => a -> Conj a -> Bool
+pertenece :: Eq a => a -> Conj a -> Bool
+pertenece v (CConj lista) = v `elem` lista
 
--- eliminar :: Eq a => a -> Conj a -> Conj a
+eliminar :: Eq a => a -> Conj a -> Conj a
+eliminar v (CConj lista) = CConj (delete v lista)
 
 -- Show??
 
 -- CHANGELOG:
 -- 1. Armo la función Vacio.
--- 2. Se insertan elementos en el Set
+-- 2. Se insertan elementos en el Set.
+-- 3. Agrego implementacion de funcion pertenece.
+-- 4. Se puede eliminar del Set.
